3d96f242c466480c68e45f66aa6dbc7083ba7b45
winston <hey@winston.sh>
2024-03-05 23:43:45 +0100

    feat: init

---
+++ main.ts
@@ -0,0 +1,72 @@
+#!/usr/bin/env -S deno run --allow-write=lints.json
+import clippyLints from "https://rust-lang.github.io/rust-clippy/master/lints.json" with {
+  type: "json",
+};
+
+const lintLevels = ["allow", "warn", "deny", "forbid"];
+
+const lintGroups: string[] = [];
+Object.values(clippyLints).forEach((lint) => {
+  if (!lintGroups.includes(lint.group)) {
+    lintGroups.push(lint.group);
+  }
+});
+lintGroups.sort();
+
+const lints = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "$id": "https://json.schemastore.org/cargo-lints.json",
+  title: "Cargo.toml",
+  type: "object",
+  definitions: {
+    "LintLevel": {
+      title: "Lint Level",
+      anyOf: [
+        {
+          type: "string",
+          enum: lintLevels,
+        },
+        {
+          type: "object",
+          properties: {
+            level: {
+              type: "string",
+              enum: lintLevels,
+            },
+            priority: {
+              type: "number",
+            },
+          },
+          required: ["level"],
+          additionalProperties: false,
+        },
+      ],
+    },
+  },
+  properties: {
+    clippy: {
+      title: "Clippy Lints",
+      type: "object",
+      properties: Object.values(clippyLints).map((lint) => ({
+        [lint.id]: {
+          description: lint.docs,
+          $ref: "#/definitions/LintLevel",
+          "x-taplo": {
+            "links": {
+              "key":
+                `https://rust-lang.github.io/rust-clippy/master/index.html#/${lint.id}`,
+            },
+          },
+        },
+      })).reduce((acc, cur) => ({ ...acc, ...cur }), {}),
+    },
+  },
+  "x-taplo-info": {
+    authors: ["nekowinston (https://github.com/nekowinston)"],
+  },
+};
+
+Deno.writeTextFileSync(
+  "cargo-lints.schema.json",
+  JSON.stringify(lints, null, 2),
+);
