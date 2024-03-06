036008fa0b93cece6370b3057394e67e16b013fa
winston <hey@winston.sh>
2024-03-06 01:07:39 +0100

    feat: more docs

--- main.ts
+++ main.ts
@@ -16,7 +16,51 @@ lintGroups.sort();
 const lints = {
   $schema: "http://json-schema.org/draft-07/schema#",
   $id: "https://json.schemastore.org/cargo-lints.json",
-  title: "Cargo.toml",
+  title: "Cargo Manifest Lints",
+  description: `#### The \`lints\` section
+
+Override the default level of lints from different tools by assigning them to a new level in a
+table, for example:
+\`\`\`toml
+[lints.rust]
+unsafe_code = "forbid"
+\`\`\`
+
+This is short-hand for:
+\`\`\`toml
+[lints.rust]
+unsafe_code = { level = "forbid", priority = 0 }
+\`\`\`
+
+\`level\` corresponds to the [lint levels](https://doc.rust-lang.org/rustc/lints/levels.html) in \`rustc\`:
+- \`forbid\`
+- \`deny\`
+- \`warn\`
+- \`allow\`
+
+\`priority\` is a signed integer that controls which lints or lint groups override other lint groups:
+- lower (particularly negative) numbers have lower priority, being overridden
+  by higher numbers, and show up first on the command-line to tools like
+  \`rustc\`
+
+To know which table under \`[lints]\` a particular lint belongs under, it is the part before \`::\` in the lint
+name.  If there isn't a \`::\`, then the tool is \`rust\`.  For example a warning
+about \`unsafe_code\` would be \`lints.rust.unsafe_code\` but a lint about
+\`clippy::enum_glob_use\` would be \`lints.clippy.enum_glob_use\`.
+
+For example:
+\`\`\`toml
+[lints.rust]
+unsafe_code = "forbid"
+
+[lints.clippy]
+enum_glob_use = "deny"
+\`\`\`
+
+Generally, these will only affect local development of the current package.
+Cargo only applies these to the current package and not to dependencies.
+As for dependents, Cargo suppresses lints from non-path dependencies with features like
+[\`--cap-lints\`](../../rustc/lints/levels.html#capping-lints).`,
   type: "object",
   definitions: {
     "LintLevel": {
@@ -24,7 +68,6 @@ const lints = {
       additionalProperties: false,
       anyOf: [
         {
-          type: "string",
           enum: lintLevels,
           "x-taplo": {
             docs: {
@@ -96,6 +139,10 @@ const lints = {
     },
   },
   "x-taplo-info": {
+    links: {
+      key:
+        "https://doc.rust-lang.org/cargo/reference/manifest.html#the-lints-section",
+    },
     authors: ["nekowinston (https://github.com/nekowinston)"],
   },
 };
